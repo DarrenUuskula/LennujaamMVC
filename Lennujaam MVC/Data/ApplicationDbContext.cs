af1f8a0a735ba4e596a487e374a0479495c52a67
DarrenUuskula <darren.uus@gmail.com>
2023-06-12 21:58:21 +0300

    add lend model in dbcontext

--- Lennujaam MVC/Data/ApplicationDbContext.cs
+++ Lennujaam MVC/Data/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Lennujaam_MVC.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Lennujaam_MVC.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Lend> Lennud { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
